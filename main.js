f286b51ed0c8a086ab4ef986c7ea558b2a241487
Antonio Guerriere <antonioguerriere@gmail.com>
2023-08-17 00:36:37 +0200

    added all basic functionality and changed background color

--- main.js
+++ main.js
@@ -8,11 +8,25 @@ for (let index = 0; index < 4264; index++) {
   week.classList.add("week")
 }
 
+const weeks = document.querySelectorAll(".week")
+
+function calculateWeeks() {
+  const today = new Date()
+  const birthday = new Date(birthdayInput.value)
+  var dif = Math.round(today-birthday);
+  return (Math.round(dif/1000/60/60/24/7));
+}
+
+
+
 birthdayInput.addEventListener("change", () => {
-  console.log('yoo')
-  const weeks = document.querySelectorAll(".week")
-  for (let index = 0; index < 700; index++) {
-    weeks[index].classList.add("highlight")
+  // clean previous highlights
+  for (let index = 0; index < 4264; index++) {
+    weeks[index].classList.remove("highlight")
     
+  }
+  // set new hightlights
+  for (let index = 0; index < calculateWeeks(); index++) {
+    weeks[index].classList.add("highlight")
   }
 })
